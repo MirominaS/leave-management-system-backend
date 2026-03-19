70029d1c2d7190a5d5592f66b03d50d5b8618bbf
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-19 14:10:13 +0530

    routes have been updated for reject, approve, cancel and login

--- routes/routes.go
+++ routes/routes.go
@@ -8,13 +8,19 @@ import (
 func RegisterRoutes() *http.ServeMux{
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/login",handlers.Login)
+
 	mux.HandleFunc("/employees", handlers.GetEmployees)
 	mux.HandleFunc("/employees/create", handlers.CreateEmployee)
 
 	mux.HandleFunc("/leave", handlers.GetLeaves)
 	mux.HandleFunc("/leave/create", handlers.CreateLeave)
+	mux.HandleFunc("/leave/cancel",handlers.CancelLeave)
+	mux.HandleFunc("/leave/approve",handlers.ApproveLeave)
+	mux.HandleFunc("/leave/reject",handlers.RejectLeave)
+
 
 	mux.HandleFunc("/dashboard", handlers.GetDashboard)
 	
-	return mux	
+	return mux
 }
