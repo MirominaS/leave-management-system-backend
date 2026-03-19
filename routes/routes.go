e2574d8cf1af3391c8174eb2c32fb75f83cc6ea2
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-20 00:58:44 +0530

    api routes added for the handlers

--- routes/routes.go
+++ routes/routes.go
@@ -12,12 +12,14 @@ func RegisterRoutes() *http.ServeMux{
 
 	mux.HandleFunc("/employees", handlers.GetEmployees)
 	mux.HandleFunc("/employees/create", handlers.CreateEmployee)
+	mux.HandleFunc("/employees/profile", handlers.GetProfile)
 
 	mux.HandleFunc("/leave", handlers.GetLeaves)
 	mux.HandleFunc("/leave/create", handlers.CreateLeave)
 	mux.HandleFunc("/leave/cancel",handlers.CancelLeave)
 	mux.HandleFunc("/leave/approve",handlers.ApproveLeave)
 	mux.HandleFunc("/leave/reject",handlers.RejectLeave)
+	mux.HandleFunc("/leave-types",handlers.GetLeaveTypes)
 
 
 	mux.HandleFunc("/dashboard", handlers.GetDashboard)
