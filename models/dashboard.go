d05e37ef91121039cb0a94ceac50327abbb3e068
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-20 18:21:51 +0530

    created Activity struct

--- models/dashboard.go
+++ models/dashboard.go
@@ -6,7 +6,12 @@ type RoleStatus struct{
 	Active int `json:"active"`
 	OnLeave int `json:"onLeave"`
 }
-
+type Activity struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Status string `json:"status"`
+	CreatedAt string `json:"created_at"`
+}
 type Dashboard struct {
 	TotalEmployees int `json:"total_employees"`
 	PendingLeaves  int `json:"pending_leaves"`
