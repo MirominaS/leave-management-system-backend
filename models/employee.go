0e73835f26f33fcf1b28836d6ae225877d6d4e4f
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-20 13:21:27 +0530

    space removed

--- models/employee.go
+++ models/employee.go
@@ -4,6 +4,6 @@ type Employee struct{
 	ID int `json:"id"`
 	Name string `json:"name"`
 	Email string `json:"email"`
-	Password string `json: "password"`
+	Password string `json:"password"`
 	RoleID int `json:"role_id"`
 }
